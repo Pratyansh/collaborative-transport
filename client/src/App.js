7a3f4dfa05b4c1c687718bb4458d902b7f194816
Pratyansh <erpratyansh@gmail.com>
2020-10-20 00:05:36 +0530

    Added frontent react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
+      </Router>
     );
   }
 }

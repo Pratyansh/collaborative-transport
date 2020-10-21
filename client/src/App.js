42a1b7b2cbb10591a6bc80647615bc82d98a597f
Pratyansh <erpratyansh@gmail.com>
2020-10-22 02:10:27 +0530

    Added redux states

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -11,14 +13,16 @@ import Register from "./components/auth/Register";
 class App extends Component {
   render() {
     return (
-      <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/errorReducers.js
@@ -0,0 +1,17 @@
+export default function(state = initialState, action) {
+    switch (action.type) {
+      case SET_CURRENT_USER:
+        return {
+          ...state,
+          isAuthenticated: !isEmpty(action.payload),
+          user: action.payload
+        };
+      case USER_LOADING:
+        return {
+          ...state,
+          loading: true
+        };
+      default:
+        return state;
+    }
+  }

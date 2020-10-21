69b09b7373046851e8ddbebad7ea0b1332e57cc1
Pratyansh <erpratyansh@gmail.com>
2020-10-22 02:26:04 +0530

    Added file for major actions

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { SET_CURRENT_USER, USER_LOADING, SET_CURRENT_USER } from "./types";
+
+//Register User
+
+//Login User
+

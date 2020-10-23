753f0a558b28f13b73e508ae066c7b02977d81fe
Pratyansh <erpratyansh@gmail.com>
2020-10-23 22:08:30 +0530

    Added login/logout/register auth actions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,9 +2,60 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
-import { SET_CURRENT_USER, USER_LOADING, SET_CURRENT_USER } from "./types";
+import { SET_CURRENT_USER, USER_LOADING, SET_CURRENT_USER, GET_ERRORS } from "./types";
 
 //Register User
+export const registerUser = (userData, history) => dispatch => {
+    axios.post("/api/users/register", userData)
+    .then(res => {
+        history.push("/login");
+    })
+    .catch (err => {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    });
+};
 
-//Login User
+//Login User - token required
 
+export const loginUser = (userData) => dispatch => {
+    axios.post("/api/users/login", userData)
+    .then(res=> {
+        const { token } = res.data;
+        localStorage.setItem("jwtToken",token);
+        setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+        dispatch ({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    })
+}
+
+//Log out user
+
+export const logout = () => dispatch => {
+    localStorage.removeItem(token);
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+}
+
+//Set currentlu logged in user
+
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+export const setUserLoading = () => {
+    return {
+        type: USER_LOADING
+    }
+}

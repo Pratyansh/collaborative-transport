0d0f7b7390ef6a4ce10c358d4cc6d8fbdaecb4d9
Pratyansh <erpratyansh@gmail.com>
2020-11-16 19:34:21 +0530

    Configured tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./styles.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import jwt_decode from "jwt-decode";

077751a7e5295a4886e43789afdf8e0bf079255e
Pratyansh <erpratyansh@gmail.com>
2020-12-24 01:39:36 +0530

    migrated to atlas db

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-	mongoURI: "mongodb://test2:test12345@ds115045.mlab.com:15045/login_cred",
+	//mongoURI: "mongodb://test2:test12345@ds115045.mlab.com:15045/login_cred",
+	mongoURI: "mongodb+srv://admin:admin100@ttg.udydg.mongodb.net/TTG-login_cred?retryWrites=true&w=majority",
 	secretOrKey: "test12345"
 };
 
